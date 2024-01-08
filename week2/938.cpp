2cf6cdf8cdc54366753d321565984ebfabebf8f5
Punit Kumar <tagpunitkumar@gmail.com>
2024-01-08 22:17:08 +0530

    day8

---
+++ week2/938.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int rangeSumBST(TreeNode *root, int low, int high) {
+        if (!root)
+            return 0;
+        if (root->val >= low && root->val <= high) {
+            int curr = root->val;
+        } else {
+            curr = 0;
+        }
+        int ls = rangeSumBST(root->left, low, high);
+        int rs = rangeSumBST(root->right, low, high);
+
+        return curr + ls + rs;
+    }
+};
