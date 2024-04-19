f4fb420246ed50baa53c4352611f4f9a4c1a62e7
punitkr03 <tagpunitkumar@gmail.com>
2024-04-19 21:51:16 +0530

    stl

---
+++ idk week/STL.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int basic[3] = {1, 2, 3};
+    array<int, 4> a = {1, 2,3,4};
+
+    int size = a.size();
+    cout << a.max_size();
+}
